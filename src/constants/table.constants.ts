87ecfbbe7a8bfbbba2ab40e7ccf8510b27aea2ca
JatinSri1909 <jatinmavrik007@gmail.com>
2025-09-04 00:14:38 +0530

    refactor: split contants into files

---
+++ src/constants/table.constants.ts
@@ -0,0 +1,5 @@
+// Database table names
+export const TABLES = {
+    SLACK_TOKENS: 'slack_tokens',
+    SCHEDULED_MESSAGES: 'scheduled_messages',
+  } as const;
---
+++ src/constants/uri.constants.ts
@@ -0,0 +1,23 @@
+// URIs and URLs
+export const URIS = {
+    // Slack API endpoints
+    SLACK_OAUTH_AUTHORIZE: 'https://slack.com/oauth/v2/authorize',
+    SLACK_OAUTH_ACCESS: 'https://slack.com/api/oauth.v2.access',
+    
+    // API routes
+    API_AUTH: '/api/auth',
+    API_SLACK: '/api/slack',
+    HEALTH_CHECK: '/health',
+    
+    // Auth routes
+    AUTH_SLACK: '/slack',
+    AUTH_SLACK_CALLBACK: '/slack/callback',
+    
+    // Slack routes
+    SLACK_CHANNELS: '/channels/:teamId',
+    SLACK_SEND_MESSAGE: '/send-message',
+    SLACK_SCHEDULE_MESSAGE: '/schedule-message',
+    SLACK_SCHEDULED_MESSAGES: '/scheduled-messages/:teamId',
+    SLACK_CANCEL_MESSAGE: '/scheduled-messages/:messageId',
+    SLACK_DB_STATUS: '/db-status',
+  } as const;
